528ddaa1dfc76cb712656befe0489d6a700a824d
Ricardo Amaya <ramaya@acr.org>
2024-03-22 10:44:56 -0400

    AILAB-3588: implementation of federated network api contracts

---
+++ src/Acr.Dart.FederatedNetwork.Api.Contract/Enums/FederatedJobStatus.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Acr.Dart.FederatedNetwork.Api.Contract.Enums
+{
+    public enum FederatedJobStatus
+    {
+        Pending,
+        Initiated,
+        PreparingDataset,
+        InProgress,
+        Completed,
+        Failed,
+        NoDataFound,
+        CancelRequested,
+        Canceled
+    }
+}
